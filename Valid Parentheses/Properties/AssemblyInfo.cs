a3c616791f9b34096042cd51fdc182d7673bd392
dlgs5100 <dlgs5100@gmail.com>
2017-08-31 11:09:45 +0800

    first commit

---
+++ Valid Parentheses/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Valid Parentheses")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Valid Parentheses")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("a4ae0600-d289-48c0-9c98-4b6c738123ab")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Valid Parentheses/UnitTest1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using NUnit.Framework;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Valid_Parentheses
+{
+    [TestFixture]
+    public class SolutionTest
+    {
+        [Test]
+        public void SampleTest1()
+        {
+            Assert.AreEqual(true, Parentheses.ValidParentheses("()"));
+        }
+
+        [Test]
+        public void SampleTest2()
+        {
+            Assert.AreEqual(false, Parentheses.ValidParentheses(")(((("));
+        }
+    }
+    public class Parentheses
+    {
+        public static bool ValidParentheses(string input)
+        {
+            var stack = new Stack<int>();
+            foreach (var c in input)
+            {
+                if (c == '(')
+                    stack.Push(1);
+                else if (c == ')')
+                {
+                    if (stack.Count == 0)
+                        return false;
+                    stack.Pop();
+                }
+            } 
+            return !stack.Any();
+        }
+    }
+}
